38b5636999a6079d5e22b809eed7b8f2e38989d6
Nate Cadicamo <natecadicamo@Nates-MacBook-Pro-3.local>
2024-08-22 13:48:46 -0700

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
@@ -9,16 +9,16 @@ const App = () => {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <GridItem area="main" bg="dodgerblue">
         Main
