5236316f331b0cf00fb8a936eae0276752e58d4d
Nate Cadicamo <natecadicamo@Nates-MacBook-Pro-3.local>
2024-08-22 14:00:32 -0700

    build color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,12 @@ const App = () => {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 };
